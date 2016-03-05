c9452b489110ba3d5900548b9f4d1ac1a49a6ae4
Robert Johnson <robertjosephjohnson@g.harvard.edu>
2016-03-05 17:17:02 -0500

    Likely last commit on the write-up for today

--- practical2.py
+++ practical2.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+#from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 from util import write_predictions
 from feature_extraction import categorization_accuracy
